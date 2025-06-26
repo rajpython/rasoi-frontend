69533e15af9f77a420f62f20f24cfc1acf47a538
Singh <rsingh@AML6GPPFP9R-11659.local>
2025-06-25 21:47:20 -0500

    homepage footer copyright year  change

--- srvemon/src/components/Footer/Footer.js
+++ srvemon/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css"
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Dhanno Banno Ki Rasoi. All rights reserved.</p>
+      <p>&copy; 1921 Dhanno Banno Ki Rasoi. All rights reserved.</p>
       <ul className="footer-links">
         <li><a href="https://www.facebook.com/profile.php?id=100015453900888">Facebook</a></li>
         <li><a href="https://www.instagram.com/reel/Cr-w8GQpyGb/">Instagram</a></li>
